ab2d1258fd78e6fc44f87610321574ca3c936269
zwpdbh <hyperion_z@outlook.com>
2017-05-11 13:09:32 +1200

    <OOP>

--- planeFinder.cpp
+++ planeFinder.cpp
@@ -60,11 +60,15 @@ int main(int argc, char *argv[]) {
         std::cout << "Could not read PLY data from file " << inputFile << std::endl;
         return -1;
     }
-    std::cout << "Read " << ply.size() << " points" << std::endl;
+
+    unsigned long totalSize = ply.size();
+    unsigned long fittedSize = 0;
+
+    std::cout << "Read " << totalSize << " points" << std::endl;
 
     // put all Ply points' associated index as data set.
     unordered_map<long, PlyPoint *> dataSet;
-    for (long i = 0; i < ply.size(); i++) {
+    for (long i = 0; i < totalSize; i++) {
         dataSet[i] = &ply[i];
     }
 
@@ -96,7 +100,7 @@ int main(int argc, char *argv[]) {
             vector<PlyPoint *> randomPoints = getRandomPoints(&dataSet);
 
             Plane currentPlane(randomPoints[0], randomPoints[1], randomPoints[2]);
-            vector<long> currentInliers = currentPlane.fitPlane(dataSet, 0.05);
+            vector<long> currentInliers = currentPlane.fitPlane(dataSet, threshold);
 
             if (currentInliers.size() > bestInliers.size()) {
                 bestInliers = currentInliers;
@@ -114,8 +118,9 @@ int main(int argc, char *argv[]) {
 
 
         planes.push_back(bestPlane);
+        fittedSize += bestPlane.inliers.size();
 
-        // update plane color, also update the data set index
+        // update plane color, also get rid of the fitted plane points
         cout << "the current bestInliers' size is: " << bestInliers.size() << endl;
         cout << "Before update data set, the current data set size is: " << dataSet.size() << endl;
 
@@ -124,15 +129,18 @@ int main(int argc, char *argv[]) {
             dataSet[each]->colour = colour;
             dataSet.erase(each);
         }
-
-        cout << "After update data set, the current data set size is: " << dataSet.size() << endl;
+        cout << "Fitted " << (double) fittedSize / totalSize << " of total points" << endl;
+        cout << "After update data set, the rest data set size is: " << dataSet.size() << endl;
     }
 
 
+    // summary
+    cout << endl;
     cout << "Fit total " << planes.size() << " planes, their size are: " << endl;
     for (int i = 0; i < planes.size(); ++i) {
         cout << "plane " << i+1 << " size: " << planes[i].inliers.size() << endl;
     }
+    cout << "Total points size: " << totalSize << ", fitted " << fittedSize << " , " << (double) fittedSize / totalSize << endl;
 
     // Write the resulting (re-coloured) point cloud to a PLY file.
     std::cout << "Writing PLY data to " << outputFile << std::endl;
